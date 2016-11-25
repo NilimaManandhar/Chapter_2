e13c5f03fb7f65c8486777f67acb84e127017b2b
NilimaManandhar <nilimamanandh@gmail.com>
2016-11-24 18:46:44 -0600

    Create pc3.java

---
+++ pc3.java
@@ -0,0 +1,31 @@
+package chapterone;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nilima M
+ * 3. Personal Information
+Write a program that displays the following information, each on a separate line:
+l Your name
+l Your address, with city, state, and ZIP
+* Your telephone number
+l Your college major
+Although these items should be displayed on separate output lines, use only a single println
+statement in your program.
+ * 
+ */
+public class pc3 {
+    public static void main(String[] args)
+    {
+        System.out.println("Name : John Smith\n"+
+                "Address : 154 RavesWood St, Chicago, IL\n"+
+                "Telephone Number : (773)-455-555\n"+
+                "College Major : Computer Science");
+    }
+    
+}
